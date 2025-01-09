1ae138f18a1f64e6845fdc3ff908eef3dc8f01c0
Phillip Bement <phillipb@pbement.com>
2025-01-09 15:46:04 -0800

    latest pyopengl fixes numpy compatibility issue

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
   name="atoms_display",
   py_modules=["atoms_display"],
-  install_requires=["numpy==1.26.3", "pyopengl", "PyOpenGL_accelerate"],
+  install_requires=["numpy", "pyopengl", "PyOpenGL_accelerate"],
+  # pyopengl lives here: https://github.com/mcfletch/pyopengl
   # Metadata:
-  version="0.4",
+  version="0.5",
   description="display a rotatable 3d visualization of molecules with the ability to animate dynamic changes",
   author="pb1729",
   url="https://github.com/pb1729/atoms-display")
